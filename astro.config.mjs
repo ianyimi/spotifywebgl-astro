1ac0b95afc5dc3d2b64c785f683f403257f77860
ianyimi <anyiminene@gmail.com>
2023-10-18 20:01:05 -0700

    fix: build fix for vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,12 +6,30 @@ import path from 'path';
 import vercel from '@astrojs/vercel/serverless';
 import { fileURLToPath } from 'url';
 import glsl from 'vite-plugin-glsl';
-
+import compress from 'astro-compress';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), auth()],
+  integrations: [
+    react(),
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    auth(),
+    compress({
+      css: true,
+      html: {
+        removeAttributeQuotes: false,
+      },
+      img: false,
+      js: true,
+      svg: false,
+      logger: 1,
+    }),
+  ],
   output: 'server',
   adapter: vercel(),
   vite: {
